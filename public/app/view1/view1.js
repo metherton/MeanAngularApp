f9f680496e4d0f22db8e4c2fccbd163be64af5ec
metherton <martinetherton63@googlemail.com>
2015-08-23 22:43:41 +0200

    use 1 controller

--- public/app/view1/view1.js
+++ public/app/view1/view1.js
@@ -5,8 +5,8 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/viewF.html',
-    controller: 'ViewGCtrl'
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
   });
 }])
 
