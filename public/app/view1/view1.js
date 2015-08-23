971016221e122553c825130fb964a739b60759a8
metherton <martinetherton63@googlemail.com>
2015-08-23 21:45:43 +0200

    use view B

--- public/app/view1/view1.js
+++ public/app/view1/view1.js
@@ -5,7 +5,7 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
-    templateUrl: 'view1/viewY.html',
+    templateUrl: 'view1/viewB.html',
     controller: 'View1Ctrl'
   });
 }])
