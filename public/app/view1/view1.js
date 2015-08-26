03885bf830a0bad96bcb59617abdbe44b8eaecf0
metherton <martinetherton63@googlemail.com>
2015-08-27 00:08:12 +0200

    add preview

--- public/app/view1/view1.js
+++ public/app/view1/view1.js
@@ -6,6 +6,7 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
+    controllerAs: 'vm',
     controller: 'View1Ctrl'
   });
 }])
@@ -13,6 +14,26 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', View1Ctrl);
 
 function View1Ctrl() {
+
+  var vm = this;
+  var previewMedia;
+
+
+  vm.preview = function() {
+    if (!previewMedia) {
+      previewMedia = new Twilio.LocalMedia();
+      Twilio.getUserMedia().then(
+          function (mediaStream) {
+            previewMedia.addStream(mediaStream);
+            previewMedia.attach('#local-media');
+          },
+          function (error) {
+            console.error('Unable to access local media', error);
+            log('Unable to access Camera and Microphone');
+          }
+      );
+    };
+  };
   
 
 
