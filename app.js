8b9b5cf9f64e29d53bae1c89c6118142044297a9
metherton <martinetherton63@googlemail.com>
2015-08-28 06:58:11 +0200

    added agents

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var agents = require('./routes/agents');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/agents', agents);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/agents.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var twilio = require('twilio');
+
+var TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN,
+    TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+
+/* GET agents listing. */
+router.get('/:who', function(req, res, next) {
+  console.log('who', req.params.who);
+  // You will need your Account Sid and a SigningKey Sid and Secret
+// to generate an Access Token for your SDK endpoint to connect to Twilio.
+  var accountSid = 'xx';
+  var signingKeySid = 'yy';
+  var signingKeySecret = 'zz';
+
+  var token = new twilio.AccessToken(signingKeySid, accountSid, signingKeySecret);
+  token.addEndpointGrant(req.params.who);
+  token.enableNTS();
+  console.log(token.toJwt());
+  res.send({token: token.toJwt()});
+});
+
+module.exports = router;
